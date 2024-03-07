b7aa07a755161889e8b0337ef1b70f8268ad3a8b
Miguel Piñeiro <129214520+PineMi@users.noreply.github.com>
2024-03-07 09:05:49 -0300

    Update Ex4AllDivisors.c

--- Programming2_06032024/Ex4AllDivisors.c
+++ Programming2_06032024/Ex4AllDivisors.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-void main(){
+int main(){
 	int num, i;
-		
+	int count = 0;
+
 	printf("Insert a Number to get all of it's dividers: ");
 	scanf("%d", &num);
 	for (i=1; i<num; i++){
 		if (num % i == 0){
 			printf("%d ", i);
+			count += i;
 		}
 	}
-	
+	if (count == num){
+	    printf("\n%d is a perfect number!", num);
+	}
+    return 0;
 }
